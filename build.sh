c070cd47e7884f53fdf32a33e9ce7a6e27385f7c
Manatsu Takahashi <manatsu@your-diary.com>
2020-05-17 17:33:30 +0900

    Added `build.sh`.

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -E
+
+if [[ $1 == "-h" || $1 == "--help" ]]; then
+    echo "Usage: ./build.sh [--install]"
+    exit 0
+fi
+
+mkdir -p build
+cd build
+cmake ..  -DCMAKE_INSTALL_PREFIX:PATH=/usr
+make
+
+if [[ $1 == "--install" ]]; then
+    sudo make install
+else
+    ./ympd
+fi
+
+cd ..
+
